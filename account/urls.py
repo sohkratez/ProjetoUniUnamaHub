5f59bdf262d0458880c3e9457cbd142d7f9b3360
vinicius_lozano <viniciuslozanodeolivera@hotmail.com>
2025-03-25 16:46:44 -0700

    mandando para avaliacao

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path 
+from django.urls import path, include
+from .views import CreateUserView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', CreateUserView.as_view(), name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from .forms import CreateUserForm
+
+class CreateUserView(FormView):
+    template_name = 'registration/user_register.html'
+    form_class = CreateUserForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
 
 # Create your views here.
